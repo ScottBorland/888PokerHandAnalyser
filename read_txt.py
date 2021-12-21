5acf83f17aa1e97cd44dfeea37a6f68ac0acf484
ScottBorland <scottborland0@gmail.com>
2021-12-21 16:20:03 +0000

    Add splitHands function which takes in list of filtered lines and returns a list of Hand objects with correct properties from file

--- read_txt.py
+++ read_txt.py
@@ -6,17 +6,80 @@ def readFileAndFilter(filePath):
 
     filteredLines = []
     for line in lines:
-        if len(line) > 2 and line[0] != "$" and line [0] != "#":
+        if len(line) > 2 and line[0] != "$" and line [0] != "#" and line[3] != "*":
             filteredLines.append(line)
     return filteredLines
 
-def 
+def splitHands(filteredLines):
+    fl = filteredLines
+    handNumber = 0
+    hands = []
+    for i in range(0, len(fl)-1):
+        #print(fl[i])
+        words = fl[i].split()
+        if words[0] == "Table":
+            handNumber += 1
+            #assign hand number
+            number = handNumber
+            #assign table name
+            table = words[1]
+            #assign players list
+            player1line = fl[i+3]
+            l = player1line.split()
+            player1 = l[2]
+            player1stack = l[4]
+            
+            player2line = fl[i+4]
+            l = player2line.split()
+            player2 = l[2]
+            player2stack = l[4]
+            
+            player3line = fl[i+5]
+            l = player3line.split()
+            player3 = l[2]
+            player3stack = l[4]
+            
+            player4line = fl[i+6]
+            l = player4line.split()
+            player4 = l[2]
+            player4stack = l[4]
+            
+            player5line = fl[i+7]
+            l = player5line.split()
+            player5 = l[2]
+            player5stack = l[4]
+            
+            player6line = fl[i+8]
+            l = player6line.split()
+            player6 = l[2]
+            player6stack = l[4]
+
+            playerInfo = {player1:player1stack, player2:player2stack, player3:player3stack, player4:player4stack, player5:player5stack, player6:player6stack}
+            #print(playerInfo)
+            
+            #assign events list
+            events = []
+            for j in range(9, len(fl)-1):
+                words = fl[j].split()
+                if words[0] == "Table":
+                    break
+                else:
+                    event = fl[j].strip()
+                    events.append(event)
+            #print(eventsList)
+
+            newHand = Hand(number, table, playerInfo, events)
+            hands.append(newHand)
+    return hands
+                
+    
 
 class Hand( object ):
-    def __init__(self, number, table, players, events):
+    def __init__(self, number, table, playerInfo, events):
         self.number = number
         self.table = table
-        self.players = players
+        self.playerInfo = playerInfo
         self.events = events
 
-readFileAndFilter('sampleFile.txt')
+filteredLines = readFileAndFilter('sampleFile.txt')
+hands = splitHands(filteredLines)
